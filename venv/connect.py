ccc658ad5ab4ca22f0c6ce1297d990ce129a37d3
didi3762 <7127821@gmail.com>
2020-05-07 21:09:47 +0300

    creating tables

--- venv/connect.py
+++ venv/connect.py
@@ -13,7 +13,35 @@ try:
     conn  = psycopg2.connect(conn_string)
     print("Connection established")
 except:
-    logging.error("select failed")
+    logging.error("connection failed")
 
+
+cursor = conn.cursor()
+
+# Create a table
+try:
+    cursor.execute("CREATE TABLE all_docs(doc_id serial PRIMARY KEY, docs TEXT, label VARCHAR);")
+    print("Finished creating table all_doc")
+except:
+    logging.error("creating table all_doc failed")
+
+try:
+    cursor.execute("CREATE TABLE tf(term VARCHAR, score FLOAT);")
+    print("Finished creating table tf")
+except:
+    logging.error("creating table tf failed")
+
+try:
+    cursor.execute("CREATE TABLE idf(doc_id INT, term VARCHAR, score FLOAT);")
+    print("Finished creating table idf")
+except:
+    logging.error("creating table idf failed")
+
+try:
+    cursor.execute("CREATE TABLE score(term VARCHAR, sport FLOAT, machine FLOAT, other FLOAT);")
+    print("Finished creating table score")
+except:
+    logging.error("creating table score failed")
 conn.commit()
+cursor.close()
 conn.close()
