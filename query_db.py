d14da4ecae51a59d000e5bada5f52e713afd5dc2
didi3762 <60739835+didi3762@users.noreply.github.com>
2020-05-10 05:26:46 +0300

    Shortcut functions

--- query_db.py
+++ query_db.py
@@ -3,30 +3,30 @@ import logging
 import connect
 from collections import namedtuple
 cur = connect.cursor;
-def create_record(obj, fields):
-    ''' given obj from db returns named tuple with fields mapped to values '''
-    Score = namedtuple("Score", fields)
-    mappings = dict(zip(fields, obj))
-    return Score(**mappings)
-def save_docs(docs,label):
-        cur.execute(f"INSERT INTO all_docs (docs, label) VALUES (%s, %s);", docs, label))
+
+def save_docs(doc,label):
+        connect.insert("all_docs","doc",doc)
+        connect.insert("all_docs", "label", doc)
+
+
+def get_docs():
+        connect.print_tables(cur,"""SELECT * FROM all_docs""")
+
+def get_doc_by_label(label):
+         connect.print_tables(cur,f"""SELECT docs, label FROM all_docs WHERE label= '{label}';""")
+
 def save_tfidf(list_of_dictionaries):
     for dictionaries in list_of_dictionaries:
-        key = list(dictionaries.keys())[0]
-        cur.execute(f"""SELECT term FROM score WHERE term = '{key}';""")
+        term = list(dictionaries.keys())[0]
+        cur.execute(f"""SELECT term FROM score WHERE term = '{term}';""")
         if cur.rowcount == 0:
-            connect.insert("score", key, dictionaries[key][1],dictionaries[key][0])
+            cur.execute(f"INSERT INTO score (term,{dictionaries[term][0]}) VALUES ({term},{dictionaries[term][1]}) ")
         else:
-            connect.update("score", key,dictionaries[key][0], dictionaries[key][1])
+            connect.update_were("score", dictionaries[term][0] ,dictionaries[term][1] ,"term",term )
 #
 def get_tfidf(**label):
     if not label:
-        cur.execute("""SELECT * FROM score""")
-        colnames = [desc[0] for desc in cur.description]
-        rows = cur.fetchall()
-        result = []
-        [result.append(create_record(row, colnames)) for row in rows]
-        print(result)
+        connect.print_tables(cur,"""SELECT * FROM score""")
     else:
         t = list(label.keys())
         for i in t:
@@ -37,28 +37,22 @@ def get_tfidf(**label):
                     logging.error("select failed")
                 if cur.rowcount > 0 :
                     if len(label)==1:
-                        colnames = [desc[0] for desc in cur.description]
-                        rows = cur.fetchall()
-                        result = []
-                        [result.append(create_record(row, colnames)) for row in rows]
-                        print(result)
+                        connect.print_tables(cur,f"""SELECT * FROM score WHERE term = '{label[i]}';""")
                 else:
                     print("The term does not exist in the table")
             elif i == "label":
                 try:
-                    cur.execute(f"""SELECT term, {label[i]} FROM score;""""")
-                    colnames = [desc[0] for desc in cur.description]
-                    rows = cur.fetchall()
-                    result = []
-                    [result.append(create_record(row, colnames)) for row in rows]
-                    print(result)
+                    connect.print_tables(cur,f"""SELECT term, {label[i]} FROM score;""""")
                 except psycopg2.errors.UndefinedColumn as e:
                     print(e)
             else:
                 print(f"{i} is not key Only label or term must be defined")
+
 def get_count_doc(label):
     cur.execute(f"""SELECT * FROM all_docs WHERE label = '{label}';""")
     return cur.rowcount
+
+
 
 connect.conn.commit()
 connect.cursor.close()
