a9c461fbb3192f409b2739554161629c7c145b30
didi3762 <60739835+didi3762@users.noreply.github.com>
2020-05-10 05:27:48 +0300

    Shortcut functions

--- venv/connect.py
+++ venv/connect.py
@@ -8,67 +8,52 @@ user = "lnhiqqex"
 password = "iP6W0C7_-6rsUI9dK7JN7WI6qxPVEx-q"
 
 # Construct connection string
-conn_string = f"host={host} user={user} dbname={dbname} password={password} "
-try:
-    conn  = psycopg2.connect(conn_string)
-    print("Connection established")
-except:
-    logging.error("connection failed")
-
-
+def connet_to_host(host,user,dbname,password):
+    conn_string = f"host={host} user={user} dbname={dbname} password={password}"
+    try:
+        conn  = psycopg2.connect(conn_string)
+        print("Connection established")
+        return conn
+    except:
+        logging.error("connection failed")
+conn = connet_to_host(host,user,dbname,password)
 cursor = conn.cursor()
 
 # Create a table
-def create_a_table():
+def create_a_table(table_name,list_of_cloum,definition):
     try:
-        cursor.execute("CREATE TABLE all_docs"
-                       "(doc_id serial PRIMARY KEY,"
-                       "docs TEXT, "
-                       "label VARCHAR);")
-        print("Finished creating table all_doc")
-    except:
-        logging.error("creating table all_doc failed")
+        cursor.execute(f"CREATE TABLE  {table_name}({list_of_cloum} {definition});")
+        print("Finished creating table")
+    except psycopg2.errors.DuplicateTable as p:
+        print(p)
 
-def create_tf_table():
+def add_cloum(table_name, cloum_name, definition):
+    cursor.execute(f"ALTER TABLE {table_name} ADD COLUMN {cloum_name} {definition};")
+
+def drop_table(cur,table_name):
+    cur.execute(f"DROP TABLE IF EXISTS {table_name};")
+    print("Finished dropping table (if existed)")
+
+def insert(table_name,cloum_name,value):
+    cursor.execute(f"INSERT INTO {table_name} ({cloum_name}) VALUES {value};")
+
+def update_were(table_name,cloum_name,value,cloum_to_comp,value_to_comp):
+    cursor.execute(f"UPDATE {table_name} SET {cloum_name} = {value} WHERE {cloum_to_comp} = '{value_to_comp}'")
+
+def print_tables(cur, select_string):
+    cur.execute(select_string)
+    colnames = [desc[0] for desc in cur.description]
+    rows = cur.fetchall()
+    result = []
+    [result.append(dict(zip(colnames, row))) for row in rows]
+    for item in result:
+        print(item)
+
+def select_all(TABLE='NEW_TABLE'):
     try:
-        cursor.execute("CREATE TABLE tf"
-                       "(term VARCHAR, "
-                       "score FLOAT);")
-        print("Finished creating table tf")
-    except:
-        logging.error("creating table tf failed")
-def create_idf_table():
-    try:
-        cursor.execute("CREATE TABLE idf"
-                       "(doc_id INT, "
-                       "term VARCHAR, "
-                       "score FLOAT);")
-        print("Finished creating table idf")
-    except:
-        logging.error("creating table idf failed")
-
-def create_score_table():
-    try:
-        cursor.execute("CREATE TABLE score"
-                       "(term VARCHAR, "
-                       "sport FLOAT,"
-                       " machine FLOAT, "
-                       "other FLOAT);")
-        print("Finished creating table score")
-    except:
-        logging.error("creating table score failed")
-
-def insert(table_name,term,score,label):
-    cursor.execute(f"INSERT INTO {table_name} (term, {label}) VALUES (%s, %s);", (term, score))
-
-def update(table_name,term,label,score):
-    cursor.execute(f"UPDATE {table_name} SET {label} = {score} WHERE term = '{term}'")
+        print_tables(cursor,f"select * from {TABLE}")
+    except psycopg2.errors.UndefinedTable as p:
+        print(p)
 
 conn.commit()
 
-
-def select_all(TABLE='NEW_TABLE'):
-    cursor.execute(f"select * from {TABLE}")
-
-select_all(TABLE= 'score')
-[print(row) for row in cursor.fetchall()]
